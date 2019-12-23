deed9cface42281e2e6bcba30e66d6d3c2e5740d
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2019-12-23 12:58:07 -0800

    Initial commit

---
+++ src/main/kotlin/com/greenback/GreenbackApplication.kt
@@ -0,0 +1,11 @@
+package com.greenback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GreenbackApplication
+
+fun main(args: Array<String>) {
+	runApplication<GreenbackApplication>(*args)
+}
---
+++ src/test/kotlin/com/greenback/GreenbackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.greenback
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GreenbackApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
