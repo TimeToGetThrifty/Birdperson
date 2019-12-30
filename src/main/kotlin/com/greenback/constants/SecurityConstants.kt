cc9731b12306a11e98946f1fc04c20428398ec92
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2019-12-29 19:58:13 -0800

    Update auth url

--- src/main/kotlin/com/greenback/constants/SecurityConstants.kt
+++ src/main/kotlin/com/greenback/constants/SecurityConstants.kt
@@ -3,7 +3,7 @@ package com.greenback.constants
 class SecurityConstants {
 
     companion object {
-        const val AUTH_LOGIN_URL = "/api/authenticate"
+        const val AUTH_LOGIN_URL = "/greenback/api/authenticate"
         const val TOKEN_HEADER = "Authorization"
         const val TOKEN_PREFIX = "Bearer "
         const val TOKEN_TYPE = "JWT"
